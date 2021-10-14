b55aac0ea4676eeba7bfd10c44b0937eb7177d20
TXRXX <toro2544@gmail.com>
2021-10-15 00:49:24 +0700

    responsive quick link

--- js/lang.js
+++ js/lang.js
@@ -2,6 +2,9 @@ const lang = document.querySelector(".slide");
 const switchLang = document.getElementById("selected-lang");
 const home = document.getElementById("home");
 const about = document.getElementById("about");
+const about2 = document.getElementById("about2");
+const education = document.getElementById("education");
+const education2 = document.getElementById("education2");
 let thai = false;
 
 lang.addEventListener("click", () => {
@@ -10,12 +13,17 @@ lang.addEventListener("click", () => {
     switchLang.textContent = "TH";
     home.textContent = "หน้าหลัก";
     about.textContent = "เกี่ยวกับ";
+    about2.textContent = "เกี่ยวกับ";
+    education.textContent = "การศึกษา";
+    education2.textContent = "การศึกษา";
   } else {
     document.querySelector(".slider").style.left = "29px";
     switchLang.textContent = "EN";
     home.textContent = "HOME";
     about.textContent = "ABOUT";
-
+    about2.textContent = "ABOUT";
+    education.textContent = "EDUCATION";
+    education2.textContent = "EDUCATION";
   }
   thai = !thai;
 })
