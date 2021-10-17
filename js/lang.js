9d3be78572ef7fc7595a4320a8b7dde79bb09ba2
Natthapong Thaboran <92025610+natthapong-t@users.noreply.github.com>
2021-10-17 23:45:18 +0700

    added research

--- js/lang.js
+++ js/lang.js
@@ -7,7 +7,40 @@ const education = document.getElementById("education");
 const education2 = document.getElementById("education2");
 const admission = document.getElementById("admission");
 const admission2 = document.getElementById("admission2");
+const admins = document.getElementById("admins");
+const tcs = document.getElementById("T-CS");
+const tit = document.getElementById("T-IT");
+const tgis = document.getElementById("T-GIS");
+const service = document.getElementById("service");
+const round2 = document.getElementById("round2");
+const round22 = document.getElementById("round2-2");
+const round3 = document.getElementById("round3");
+const researchGroup = document.getElementById("researchGroup");
+const MOU = document.getElementById("MOU");
 const contact = document.getElementById("contact");
+const weltext = document.getElementById("wel-text");
+const history = document.getElementById("history");
+
+// const research = document.getElementById("research");
+
+
+const bachelor = document.querySelectorAll("#bachelor");
+const personnel = document.querySelectorAll("#personnel");
+const syllabus = document.querySelectorAll("#syllabus");
+const scholarship = document.querySelectorAll("#scholarship");
+const calendar = document.querySelectorAll("#calendar");
+
+
+const bachelorText = document.querySelectorAll("#bachelor-text");
+const masterText = document.querySelectorAll("#master-text");
+const doctorText = document.querySelectorAll("#doctor-text");
+const research = document.querySelectorAll("#research");
+
+
+
+let arrow = document.createElement("i");
+arrow.classList.add("fas", "fa-caret-right");
+
 let thai = false;
 
 lang.addEventListener("click", () => {
@@ -21,7 +54,36 @@ lang.addEventListener("click", () => {
     education2.textContent = "การศึกษา";
     admission.textContent = "การรับเข้าศึกษา";
     admission2.textContent = "การรับเข้าศึกษา";
+    admins.textContent = "คณะผู้บริหาร";
+    tcs.textContent = "คณาจารย์ประจำหลักสูตร CS";
+    tit.textContent = "คณาจารย์ประจำหลักสูตร IT";
+    tgis.textContent = "คณาจารย์ประจำหลักสูตร GIS";
+    service.textContent = "บริการการศึกษา";
+    round2.textContent = "รอบที่ 2";
+    round22.textContent = "รอบที่ 2.2";
+    round3.textContent = "รอบที่ 3";
+    researchGroup.textContent = "ศูนย์วิจัยและกลุ่มวิจัย";
+    MOU.textContent = "ความร่วมมือ MOU";
     contact.textContent = "ติดต่อ";
+    history.textContent = "ประวัติ";
+    researchGroup.textContent = "งานวิจัย";
+    // document.getElementById("history-th").src="./img/banner/history_th.jpg";
+    // research.textContent = "งานวิจัย";
+    // weltext.innerHTML = "ยินดีต้อนรับสู่<br>วิทยาลัยการคอมพิวเตอร์";
+    // weltext.textContent = "ยินดีต้อนรับสู่ \r\n วิทยาลัยการคอมพิวเตอร์";
+
+
+    changeTextAndAddArrow(bachelor, "ระดับปริญญาตรี");
+    changeTextAndAddArrow(personnel, "บุคลากร");
+    changeTextAndAddArrow(syllabus, "หลักสูตร");
+    changeTextAndAddArrow(scholarship, "ทุนการศึกษา");
+    changeTextAndAddArrow(calendar, "ปฏิทินการศึกษา");
+
+    changeText(bachelorText, "ระดับปริญญาตรี");
+    changeText(masterText, "ระดับปริญญาโท");
+    changeText(doctorText, "ระดับปริญญาเอก");
+    changeText(research, "งานวิจัย");
+    changeText(grad, "ระดับบัณฑิตศึกษา");
   } else {
     document.querySelector(".slider").style.left = "29px";
     switchLang.textContent = "EN";
@@ -30,10 +92,50 @@ lang.addEventListener("click", () => {
     about2.textContent = "ABOUT";
     education.textContent = "EDUCATION";
     education2.textContent = "EDUCATION";
-    admission.textContent = "ADMISSION";
-    admission2.textContent = "ADMISSION";
+    admission.textContent = "ADDMISSION";
+    admission2.textContent = "ADDMISSION";
+    admins.textContent = "ADMINISTRATORS";
+    tcs.textContent = "TEACHING STAFF IN CS";
+    tit.textContent = "TEACHING STAFF IN IT";
+    tgis.textContent = "TEACHING STAFF IN GIS";
+    service.textContent = "SERVICE";
+    round2.textContent = "ROUND 2";
+    round22.textContent = "ROUND 2.2";
+    round3.textContent = "ROUND 3";
+    researchGroup.textContent = "RESEARCH GROUP";
+    MOU.textContent = "MOU";
     contact.textContent = "CONTACT";
+    history.textContent = "HISTORY";
+    // document.getElementById("history-th").src="./img/banner/history_en.jpg";
+    // research.textContent = "RESEARCH";
+    // weltext.innerHTML = "WELCOME TO<br>COLLEGE OF COMPUTING";
+    // weltext.textContent = "WELCOME TO\r\nCOLLEGE OF COMPUTING";
+
+
+    changeTextAndAddArrow(bachelor, "BACHELOR'S DEGREE");
+    changeTextAndAddArrow(personnel, "PERSONNEL");
+    changeTextAndAddArrow(syllabus, "SYLLABUS");
+    changeTextAndAddArrow(scholarship, "SCHOLARSHIP");
+    changeTextAndAddArrow(calendar, "CALENDAR");
+
+    changeText(bachelorText, "BACHELOR'S DEGREE");
+    changeText(masterText, "MASTER'S DEGREE");
+    changeText(doctorText, "DOCTOR'S DEGREE");
+    changeText(research, "RESEARCH");
+    changeText(grad, "GRADUATE STUDENTS");
   }
   thai = !thai;
 })
 
+const changeTextAndAddArrow = (array, textChange) => {
+  for (let i=0; i<array.length; i++) {
+    array[i].textContent = textChange;
+    array[i].appendChild(arrow.cloneNode(true));
+  }
+}
+
+const changeText = (array, textNew) => {
+  for (let i=0; i<array.length; i++) {
+    array[i].textContent = textNew;
+  }
+}
